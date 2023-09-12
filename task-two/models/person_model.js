28fb9aff0c8f355ab9aba811348be3ace8ce11f6
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-12 22:44:31 +0100

    creating model for mongodb

---
+++ task-two/models/person_model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const personSchema = mongoose.Schema(
+    {
+        name: {type: String, 
+               required: [true, "Please enter a name"]
+            },
+
+        age:{type: Number,
+             required: [false, "Please enter an age",],
+            default: 0},
+
+        track:{type:String,
+               required: [false, "Please enter a track"]
+            }
+
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Person = mongoose.model('Person', personSchema);
+
+module.exports = Person;
--- task-two/server.js
+++ task-two/server.js
@@ -13,6 +13,9 @@ app.listen(3000, ()=>{
 
 mongoose.connect('mongodb+srv://admin:admin@hng-task-two.jjsfmiz.mongodb.net/hng-task-two?retryWrites=true&w=majority')
 .then(()=>{
+    app.listen(3000,()=>{
+        console.log('HNG API is connected to MongoDB and running on port 3000')
+    })
     console.log('Connected to  Mongodb database')
 }).catch((err)=>{
     console.log(err)
