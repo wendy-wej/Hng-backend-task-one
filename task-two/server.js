3d9b0a1f780b895333b05dff5b6a96ebd01f06c5
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-12 21:30:33 +0100

    connecting to mongoose

--- task-two/server.js
+++ task-two/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
 
 //routes
 app.get('/api', (req, res) =>{
-    res.send("Hello all")
+    res.send("Hello World")
 })
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000'	)
+})
+
+mongoose.connect('mongodb+srv://admin:admin@hng-task-two.jjsfmiz.mongodb.net/hng-task-two?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('Connected to  Mongodb database')
+}).catch((err)=>{
+    console.log(err)
 })
